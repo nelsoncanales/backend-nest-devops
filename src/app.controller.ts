82681d9a02bf2b5371569f36d0262e35697d8918
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-06-02 18:43:43 -0400

    Creacion de nuevo endpoint para saludo en aleman

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,14 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // localhost:3000/
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  // localhost:3000/hallo
+  @Get('hallo')
+  getHelloAleman(): string {
+    return this.appService.getHelloAleman();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHelloAleman(): string {
+    return 'Hallo Welt';
+  }
 }
